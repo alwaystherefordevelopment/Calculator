4062072762bf0ce341afc5e4ced96c90a016d07e
Kumar Abhishek <kumarabhishek.1109@live.com>
2022-11-06 11:05:03 +0530

    Add project files.

---
+++ Calculator/CalculatorClass.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculator
+{
+    public class CalculatorClass
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
